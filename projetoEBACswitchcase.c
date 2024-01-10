e3029ea30f3274f38875f865b9af2f445eb0ecbd
mandiilorenzo <156244585+mandiilorenzo@users.noreply.github.com>
2024-01-10 17:57:44 -0300

    Opção de saída
    Incluso a opção de saída para o usuário.

--- projetoEBACswitchcase.c
+++ projetoEBACswitchcase.c
@@ -148,7 +148,8 @@ int main ()
 	    printf("Escolha a opção desejada no menu: \n\n");
 	    printf("\t1 - Registrar nomes\n");
 	    printf("\t2 - Consultar nomes\n");
-	    printf("\t3 - Deletar nomes\n\n"); 
+	    printf("\t3 - Deletar nomes\n");
+		printf("\t4 - Sair do sistema\n\n");
 	    printf ("Opção: "); //fim do menu
 	
 	    scanf ("%d", &opcao); //armazenando a escolha do usuário
@@ -167,6 +168,11 @@ int main ()
 		    
 		    case 3:
 		    deletar();
+		    break;
+		    
+		    case 4:
+		    printf("Obrigado por utilizar nosso sistema!\n");
+		    return 0; //quebra de laço/repetição/função
 		    break;
 		    
 		    default: 
